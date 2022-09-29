a33de9917e4ca32eca7b91f6d1eeb4420663beee
Marty <martynolan17@gmail.com>
2022-09-29 11:24:05 -0400

    added put and getAll to database.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -39,7 +39,7 @@ module.exports = () => {
         ]
       }),
       new InjectManifest({
-        swSrc: "./sw.js",
+        swSrc: "./src-sw.js",
         swDest: "service-worker.js",
       }),
       
