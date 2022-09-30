2eb716961b22c6587f078e07b036d3841f2142fb
Marty <martynolan17@gmail.com>
2022-09-30 11:06:17 -0400

    added asset caching

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -27,11 +27,19 @@ export const putDb = async (content) => {
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
 
+  // Create a connection to the database and version we want to use
   const jateDB = await openDB("jate", 1);
+
+  // Create a new transaction and specify the database and data privelages
   const tx = jateDB.transaction("jate", "readonly");
+
+  // Open up the descired object store
   const store = tx.objectStore("jate");
+
+  // Use the .getAll() method to get all data in the database
   const request = store.getAll();
 
+  // Get confirmation of the request
   const result = await request;
   console.log("result.value", result);
   return result;
